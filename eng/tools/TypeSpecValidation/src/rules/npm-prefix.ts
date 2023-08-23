mnadamod/azure-rest-apispecs

commit b0632399a7da8f7decd6055bcfedb2d4a2bf373b

[TypeSpec Validation] NpmPrefix: Use "git rev-parse" to find repo root (#25466)

eng/tools/TypeSpecValidation/src/rules/npm-prefix.ts
@@ -1,3 +1,4 @@
+import { simpleGit } from "simple-git";
 import { runCmd } from "../utils.js";
 import { Rule } from "../rule.js";
 import { RuleResult } from "../rule-result.js";
@@ -7,7 +8,20 @@ export class NpmPrefixRule implements Rule {
   readonly description = "Verify spec is using root level package.json";
 
   async execute(folder: string): Promise<RuleResult> {
-    let expected_npm_prefix = process.cwd();
+    const git = simpleGit(folder);
+
+    let expected_npm_prefix: string | undefined;
+    try {
+      // If spec folder is inside a git repo, returns repo root
+      expected_npm_prefix = await git.revparse("--show-toplevel");
+    } catch (err) {
+      // If spec folder is outside git repo, or if problem running git, throws error
+      return {
+        success: false,
+        errorOutput: err instanceof Error ? err.message : undefined,
+      };
+    }
+
     const actual_npm_prefix = (await runCmd(`npm prefix`, folder))[1].trim();
 
     let success = true;

eng/tools/TypeSpecValidation/src/rules/git-diff.ts
@@ -6,8 +6,8 @@ export class GitDiffRule implements Rule {
   readonly name = "GitDiff";
   readonly description = "Checks if previous rules resulted in a git diff";
 
-  async execute(): Promise<RuleResult> {
-    const git = simpleGit();
+  async execute(folder: string): Promise<RuleResult> {
+    const git = simpleGit(folder);
     let gitStatusIsClean = (await git.status(["--porcelain"])).isClean();
 
     let success = true;

commit 17f63243166cad8723bc333f21100f66d948919a

[TypeSpec Validation] NpmPrefix: Normalize paths from external tools (#25483)
- Prevents failures on Windows due to different path separators
- Fixes issue introduced in #25466

eng/tools/TypeSpecValidation/src/rules/npm-prefix.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { simpleGit } from "simple-git";
 import { runCmd } from "../utils.js";
 import { Rule } from "../rule.js";
@@ -13,7 +14,7 @@ export class NpmPrefixRule implements Rule {
     let expected_npm_prefix: string | undefined;
     try {
       // If spec folder is inside a git repo, returns repo root
-      expected_npm_prefix = await git.revparse("--show-toplevel");
+      expected_npm_prefix = path.normalize(await git.revparse("--show-toplevel"));
     } catch (err) {
       // If spec folder is outside git repo, or if problem running git, throws error
       return {
@@ -22,7 +23,7 @@ export class NpmPrefixRule implements Rule {
       };
     }
 
-    const actual_npm_prefix = (await runCmd(`npm prefix`, folder))[1].trim();
+    const actual_npm_prefix = path.normalize((await runCmd(`npm prefix`, folder))[1].trim());
 
     let success = true;
     let stdOutput =