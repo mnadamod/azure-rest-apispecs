mnadamod/azure-rest-apispecs

commit f0e3aa9679cd35f1fd9ed6f0aad98c5b83fc6c49

tsv failed when TypeSpec-Validation return 1 (#34156)
* will stop when error

* update

* up

* up

* revert

* fix

* Update eng/tools/typespec-validation/src/rules/sdk-tspconfig-validation.ts

Co-authored-by: Wanpeng Li <wanl@microsoft.com>

* up

* update

---------

Co-authored-by: Kyle Zhang <v-zhanh@microsoft.com>
Co-authored-by: Wanpeng Li <wanl@microsoft.com>

eng/tools/typespec-validation/src/rules/sdk-tspconfig-validation.ts
@@ -504,9 +504,8 @@ export class SdkTspConfigValidationRule implements Rule {
         ? `${failedResults.map((r) => r.errorOutput).join("\n")}\n Please see https://aka.ms/azsdk/spec-gen-sdk-config for more info.`
         : "";
 
-    // NOTE: to avoid huge impact on existing PRs, we always return true with info/warning messages.
     return {
-      success: true,
+      success,
       stdOutput: `[${this.name}]: validation ${success ? "passed" : "failed"}.\n${stdOutputFailedResults}`,
     };
   }

eng/tools/typespec-validation/test/sdk-tspconfig-validation.test.ts
@@ -582,7 +582,7 @@ describe("tspconfig", function () {
 
     const rule = new SdkTspConfigValidationRule(c.subRules);
     const result = await rule.execute(c.folder);
-    strictEqual(result.success, true);
+    strictEqual(result.success, c.success);
     if (c.success)
       strictEqual(result.stdOutput?.includes("[SdkTspConfigValidation]: validation passed."), true);
     if (!c.success)

commit 5421378aabfd4d5a89fd6770a8c9ac3eed48fd5e

[TypeSpecValidation] Revert SdkTspConfigValidation to warning-only (#34295)
- Fixes TSV for all exsting specs
- Reverts failures introduced by #34156

eng/tools/typespec-validation/src/rules/sdk-tspconfig-validation.ts
@@ -528,8 +528,9 @@ export class SdkTspConfigValidationRule implements Rule {
         ? `${failedResults.map((r) => r.errorOutput).join("\n")}\nPlease see https://aka.ms/azsdk/spec-gen-sdk-config for more info.\nFor additional information on TypeSpec validation, please refer to https://aka.ms/azsdk/specs/typespec-validation.`
         : "";
 
+    // NOTE: to avoid huge impact on existing PRs, we always return true with info/warning messages.
     return {
-      success: isManagementSdk(folder) ? success : true,
+      success: true,
       stdOutput: `[${this.name}]: validation ${success ? "passed" : "failed"}.\n${stdOutputFailedResults}`,
     };
   }

eng/tools/typespec-validation/test/sdk-tspconfig-validation.test.ts
@@ -597,7 +597,8 @@ describe("tspconfig", function () {
 
     const rule = new SdkTspConfigValidationRule(c.subRules);
     const result = await rule.execute(c.folder);
-    const returnSuccess = c.folder.includes(".Management") ? c.success : true;
+    // NOTE: to avoid huge impact on existing PRs, we always return true with info/warning messages.
+    const returnSuccess = true;
     strictEqual(result.success, returnSuccess);
     if (c.success)
       strictEqual(result.stdOutput?.includes("[SdkTspConfigValidation]: validation passed."), true);